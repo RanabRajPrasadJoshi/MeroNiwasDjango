8eb62b8d97e87b21f239c15d6ae5a40afed8a232
unknown <ranabjoshi20@gmail.com>
2024-06-16 08:42:38 +0545

    newchange

--- src/home/admin.py
+++ src/home/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from . models import * 
 
 # Register your models here.
-Category.objects.get_or_create(name='House')
-Category.objects.get_or_create(name='Room')
-Category.objects.get_or_create(name='Flat')
+Category.objects.get_or_create(name='HOME')
+Category.objects.get_or_create(name='ROOM')
+Category.objects.get_or_create(name='FLAT')
 admin.site.register(Room)
 admin.site.register(Category)
--- src/home/models.py
+++ src/home/models.py
@@ -1,6 +1,13 @@
 from django.db import models
+
+
 class Category(models.Model):
-    name = models.CharField(max_length=50)
+    CATEGOTY = (
+        ('HOME','HOME'),
+        ('FLAT', 'FLAT'),
+        ('ROOM', 'ROOM')
+    )
+    name = models.CharField(choices=CATEGOTY, max_length=250)
 
     def __str__(self):
         return self.name
@@ -8,7 +15,7 @@ class Category(models.Model):
 class Room(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    img = models.FileField(upload_to='uploads/')  # FileField for uploading images
+    img = models.FileField(upload_to='static/uploads')  # FileField for uploading images
     location = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
