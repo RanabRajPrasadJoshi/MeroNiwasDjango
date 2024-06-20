47c596851d5934df427f9620fc5bf4ed50b7ea83
unknown <ranabjoshi20@gmail.com>
2024-06-20 08:58:50 +0545

    new ubdate

--- src/home/urls.py
+++ src/home/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
    path('', views.show_product, name='home'),
    path('ContactUs/', views.show_contactus, name='ContactUs'),
    path('Product/', views.search, name='search'),
+   path('Login/', views.show_login, name='login'),
+   path('Register/', views.show_selectRegister, name='selectregister'),
+   path('RegisterBroker/', views.show_registerbroker, name='registerbroker'),
    
     
 ]
