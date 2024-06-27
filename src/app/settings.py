aa49b52e8531461e998531b3cfdde12668dadb17
unknown <ranabrajjoshi@gmail.com>
2024-06-28 03:35:37 +0545

    New add

--- src/app/settings.py
+++ src/app/settings.py
@@ -40,10 +40,15 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'home',
     'crispy_forms',
+    "crispy_bootstrap5",
 ]
 
 
 CRISPY_TEMPLATE_PACK = 'uni_form'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 
 MIDDLEWARE = [
--- src/home/views.py
+++ src/home/views.py
@@ -1,23 +1,18 @@
 from django.shortcuts import render , redirect , get_object_or_404
 from django.http import HttpResponse
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth import authenticate,login,logout ,  update_session_auth_hash
 from django.http import JsonResponse
 from . models import *
 import random
 from django.core.mail import send_mail
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
     return render(request, 'home/index.html')
-
-def show_product(request):
-    
-    return render(request, 'home/index.html')
-
-
 
 def show_contactus(request):
     return render(request, 'home/ContactUs.html')
@@ -258,18 +253,172 @@ def verify_emailregular(request):
 
     return render(request, 'home/verify_emailregular.html')
 
-def search(request):
-    query = request.GET.get('q')
-    category = request.GET.get('category')
+
+
+def show_product(request):
     rooms = Room.objects.all()
+
+    if request.user.is_authenticated:
+        user_profile = UserProfile.objects.get(user=request.user)
+
+        if user_profile.accountType == "broker":
+            return show_productBroker(request, rooms)
+        else:
+            return render(request, 'home/Product.html', {'rooms': rooms})
+
+    return render(request, 'home/Product.html', {'rooms': rooms})
+
+def show_productBroker(request, rooms):
+    return render(request, 'home/ProductBroker.html', {'rooms': rooms})
+
+def show_AddProduct(request):
+    if request.method == 'POST':
+        title = request.POST.get("title").strip()
+        price = request.POST.get("price").strip()
+        location = request.POST.get("location").strip()
+        description = request.POST.get("description").strip()
+        img = request.FILES.get('room-picture')
+        user = request.user.username
+
+        if not title or not price or not location or not description:
+            return HttpResponse("Please fill in all fields. Some fields are empty or only contain spaces.")
+        
+        if img:
+            file_extension = img.name.split('.')[-1].lower()
+            if file_extension not in ["jpg", "jpeg", "png", "gif"]:
+                return HttpResponse("Please upload only image files (jpg, jpeg, png, gif).")
+            
+        room = Room.objects.create(\
+            user=user,
+            title=title,
+            price=price,
+            location=location,
+            description=description,
+            img=img
+        )
+        room.save()
+        messages.add_message(request, messages.SUCCESS, 'Room Added successfully')
+        return redirect('MyProduct')
     
-    if query:
-        rooms = rooms.filter(description__icontains=query)
+
+    return render(request, 'home/AddProduct.html')
+
+def show_MyProduct(request):
+    if request.user.is_authenticated:
+        rooms = Room.objects.filter(user=request.user.username)  # Filter rooms by the logged-in user's username
+    else:
+        rooms = []
+
+    return render(request, 'home/MyProduct.html', {'rooms': rooms})
+
+
+def show_updateProduct(request, room_id):
+    instance = get_object_or_404(Room, id=room_id)
     
-    if category:
-        rooms = rooms.filter(category__name__icontains=category)
+    if request.method == "POST":
+        title = request.POST.get("title").strip()
+        price = request.POST.get("price").strip()
+        location = request.POST.get("location").strip()
+        description = request.POST.get("description").strip()
+        img = request.FILES.get('room-picture')
 
-    categories = Category.objects.all()
+        if not title or not price or not location or not description:
+            messages.add_message(request, messages.ERROR, "Please fill in all fields. Some fields are empty or only contain spaces.")
+        else:
+            instance.title = title
+            instance.price = price
+            instance.location = location
+            instance.description = description
 
-    return render(request, 'home/Product.html', {'rooms': rooms, 'categories': categories})
+            if img:
+                file_extension = img.name.split('.')[-1].lower()
+                if file_extension in ["jpg", "jpeg", "png", "gif"]:
+                    instance.room_picture = img
+                else:
+                    messages.add_message(request, messages.ERROR, "Please upload only image files (jpg, jpeg, png, gif).")
+                    return render(request, 'home/Updateproduct.html', {'room': instance})
 
+            instance.save()
+            messages.add_message(request, messages.SUCCESS, 'Room updated successfully')
+            return redirect('MyProduct')  # Redirect to the 'show_my_product' view
+    
+    return render(request, 'home/UpdateProduct.html', {'room': instance})
+
+
+def show_deleteProduct(request, room_id):
+      instance = Room.objects.get(id=room_id)
+      instance.delete()
+      messages.add_message(request, messages.SUCCESS, "Product Deleted successfully")
+      return redirect('MyProduct')
+
+
+def product_detail(request, room_id):
+    room = get_object_or_404(Room, id=room_id)
+    return render(request, 'home/room_detail.html', {'room': room})
+
+
+def show_profile(request):
+    if request.user.is_authenticated:
+        user_profile = UserProfile.objects.get(user=request.user)
+
+        context = {
+            'user_profile': user_profile,
+        }
+
+        return render(request, 'home/Profile.html', context)
+    
+    return render(request, 'home/Profile.html', {})
+
+
+
+@login_required
+def update_profile(request):
+    if request.method == 'POST':
+        password = request.POST.get('password')
+        new_password = request.POST.get('new-password')
+        confirm_new_password = request.POST.get('confirm-new-password')
+        contact_number = request.POST.get('contactnumber')
+        email = request.POST.get('email')
+        gender = request.POST.get('gender')
+        profile_picture = request.FILES.get('profile-picture')
+
+        user = request.user
+
+        # Check if the provided password is correct
+        if not user.check_password(password):
+            messages.error(request, 'Incorrect password. Please try again.')
+            return redirect('update_profile')  # Redirect back to update profile page
+
+        try:
+            # Retrieve UserProfile associated with the User
+            user_profile = user.userprofile
+        except UserProfile.DoesNotExist:
+            messages.error(request, 'User profile does not exist.')
+            return redirect('update_profile')  # Redirect back to update profile page
+
+        # Update UserProfile fields
+        user_profile.contact_number = contact_number
+        user_profile.gender = gender
+        if profile_picture:
+            user_profile.profile_picture = profile_picture
+        user_profile.save()
+
+        # Update User email if changed
+        if user.email != email:
+            user.email = email
+            user.save()
+
+        # Change password if new password is provided
+        if new_password:
+            if new_password != confirm_new_password:
+                messages.error(request, 'New passwords do not match. Please try again.')
+                return redirect('updateprofile')  # Redirect back to update profile page
+            
+            user.set_password(new_password)
+            user.save()
+            update_session_auth_hash(request, user)  # Update session with new password hash
+
+        messages.success(request, 'Profile updated successfully.')
+        return redirect('Profile')  # Redirect to profile view or home after successful update
+
+    return render(request, 'home/updateprofile.html', {'user': request.user})
